b5fa73c5710806f1a69a3d4f56fde66a4d2c3daf
Geovane Borba <geovanne@live.com>
2025-06-02 20:24:29 -0300

    feat: implement Question entity

---
+++ src/domain/forum/enterprise/entities/question.ts
@@ -0,0 +1,90 @@
+import dayjs from 'dayjs'
+
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/type/optional'
+import { Slug } from './value-objects/slug'
+
+interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
+  title: string
+  content: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromTitle(props.title),
+        createdAt: new Date(),
+      },
+      id
+    )
+
+    return question
+  }
+
+  get authorId(): UniqueEntityID {
+    return this.props.authorId
+  }
+
+  get bestAnswerId(): UniqueEntityID | undefined {
+    return this.props.bestAnswerId
+  }
+
+  get title(): string {
+    return this.props.title
+  }
+
+  get content(): string {
+    return this.props.content
+  }
+
+  get slug(): Slug {
+    return this.props.slug
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromTitle(title)
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+}
