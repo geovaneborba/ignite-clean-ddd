1795bd6bdf26ae81590193bfd11e24ae3ab3efbf
Geovane Borba <geovanne@live.com>
2025-06-11 17:21:28 -0300

    feat: update QuestionsRepository interface to include additional methods

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,5 +1,11 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
-export interface IQuestionsRepository {
+export interface QuestionsRepository {
+  save(question: Question): Promise<void>
   create(question: Question): Promise<void>
+  findBySlug(slug: string): Promise<Question | null>
+  findById(id: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
+  delete(question: Question): Promise<void>
 }
