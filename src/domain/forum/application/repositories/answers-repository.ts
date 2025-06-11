7c0e7d4ddc36a65f93f7a6b6f1d7fe3c0aaf7b5c
Geovane Borba <geovanne@live.com>
2025-06-11 17:17:20 -0300

    feat: update AnswersRepository interface to include additional methods

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 /**
@@ -5,6 +6,13 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
  * This interface defines the contract for the answers repository,
  * which is responsible for managing the persistence of Answer entities.
  */
-export interface IAnswersRepository {
+export interface AnswersRepository {
+  save(answer: Answer): Promise<void>
   create(answer: Answer): Promise<void>
+  delete(answer: Answer): Promise<void>
+  findById(id: string): Promise<Answer | null>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<Answer[]>
 }
