e09bdf7c12574297fb2e9b02f7d66b2abdb37e63
Geovane Borba <geovanne@live.com>
2025-06-11 17:27:13 -0300

    feat: implement Comment entity

---
+++ src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,38 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export interface CommentProps {
+  authorId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export abstract class Comments<
+  Props extends CommentProps,
+> extends Entity<Props> {
+  get authorId(): UniqueEntityID {
+    return this.props.authorId
+  }
+
+  get content(): string {
+    return this.props.content
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+}
