6cda2735934dac0b012a3d9e8d017b3a19814d20
Geovane Borba <geovanne@live.com>
2025-06-11 17:28:58 -0300

    feat: update Question entity to allow custom createdAt date

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -24,7 +24,7 @@ export class Question extends Entity<QuestionProps> {
       {
         ...props,
         slug: props.slug ?? Slug.createFromTitle(props.title),
-        createdAt: new Date(),
+        createdAt: props.createdAt ?? new Date(),
       },
       id
     )
